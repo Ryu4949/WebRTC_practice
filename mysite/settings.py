481373df57cd8e1b1d8011eb70cfb30c1fe130d1
Ryu4949 <kyong494949@gmail.com>
2022-06-28 13:00:17 +0900

    update settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'chat',
+
+    'channels',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +127,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ASGI_APPLICATION = 'mysite.asgi.application'
